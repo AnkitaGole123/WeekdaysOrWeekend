b8fa3015484cfe91ae4596e68350cc0661d0667e
Ankita <you@example.com>
2019-05-03 23:39:51 +0530

    Taking user input

---
+++ weekdaysOrWeekends.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class weekdaysOrWeekends {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username ");
+    }
+}
