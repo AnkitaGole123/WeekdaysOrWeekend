ea0cbada8e19684607347eb3fed1e9e77360868e
Ankitagole <ankita17@navgurukul.org>
2019-05-05 10:15:54 +0530

    ignoring case`

--- weekdaysOrWeekends.java
+++ weekdaysOrWeekends.java
@@ -3,5 +3,12 @@ public class weekdaysOrWeekends {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter username ");
+        String userName = myObj.next();
+         if(userName.equalsIgnoreCase("saturday") || userName.equalsIgnoreCase("sunday")) {
+             System.out.println("weekends");
+         }
+         else{
+             System.out.println("week days");
+         }
     }
 }
