06254b41bfb72f8136eedd1eb03d155ebca7e6fe
Ankitagole <ankita17@navgurukul.org>
2019-05-13 12:43:02 +0530

    changing class name

---
+++ WeekdaysWeekends.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class WeekdaysWeekends {
+    public static void main(String[] args) {
+        String days = args[0].toLowerCase();
+        Set<String> weekDays = new HashSet<>(List.of("monday","tuesday","wednesday","thursday","friday"));
+        if(getWeekends(days)) {
+            System.out.println("weekends");
+        }
+        else if(days.contains(days)){
+            System.out.println("week days ");
+        }
+        else {
+            System.out.println("invalid input");
+        }
+    }
+    private static boolean getWeekends(String weekends) {
+        return weekends.equalsIgnoreCase("saturday") || weekends.equalsIgnoreCase("sunday");
+    }
+}
