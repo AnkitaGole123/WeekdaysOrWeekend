b321c8f6c280955d56d4a162cc74487931eed3c3
Ankitagole <ankita17@navgurukul.org>
2019-05-13 13:34:55 +0530

    adding test case

--- WeekdaysWeekends.java
+++ WeekdaysWeekends.java
@@ -2,17 +2,16 @@ import java.util.*;
 public class WeekdaysWeekends {
     public static void main(String[] args) {
         String days = args[0].toLowerCase();
-        Set<String> weekDays = new HashSet<>(List.of("monday","tuesday","wednesday","thursday","friday"));
-        if(getWeekends(days)) {
+        Set<String> weekDays = new HashSet<>(List.of("monday", "tuesday", "wednesday", "thursday", "friday"));
+        if (getWeekends(days)) {
             System.out.println("weekends");
-        }
-        else if(days.contains(days)){
-            System.out.println("week days ");
-        }
-        else {
+        } else if (weekDays.contains(days)) {
+            System.out.println("week days");
+        } else {
             System.out.println("invalid input");
+        }
     }
-    private static boolean getWeekends(String weekends) {
-        return weekends.equals("saturday") || weekends.equals("sunday");
+    private static boolean getWeekends(String days) {
+        return days.equals("saturday") || days.equals("sunday");
     }
 }
---
+++ test.sh
@@ -0,0 +1,9 @@
+javac WeekdaysWeekends.java
+java WeekdaysWeekends Sunday > actual
+diff actual ExpectedOutput1
+
+java WeekdaysWeekends wednesday > actual
+diff actual ExpectedOutput2
+
+java WeekdaysWeekends pooja > actual
+diff actual ExpectedOutput3
